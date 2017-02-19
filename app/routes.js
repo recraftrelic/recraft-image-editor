41a5f71cfee5de2af27da2e9b1b70cac8659c94a
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-19 11:36:15 +0530

    setted up EditorComponent

--- app/routes.js
+++ app/routes.js
@@ -2,11 +2,11 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
+import EditorPage from './containers/EditorPage';
 
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={HomePage} />
+    <IndexRoute component={EditorPage} />
   </Route>
 );
