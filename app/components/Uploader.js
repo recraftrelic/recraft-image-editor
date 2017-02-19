0b1475ea90784ac0c1998e18df52e1c4ca2aa25b
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-19 12:36:49 +0530

    imported Uploader into editorPage

--- app/components/Uploader.js
+++ app/components/Uploader.js
@@ -11,27 +11,7 @@ export default class Uploader extends Component {
 	render() {
 		return(
 			<div className="upload-container">
-				<Dropzone className="Uploader" style={
-					{
-						width: '50%',
-						height: '80%',
-						overflow: 'auto',
-						margin: 'auto',
-						position: 'absolute',
-						top: '0',
-						left: '0',
-						bottom: '0',
-						right: '0',
-						textAlign: 'center',
-						border: '2px dashed #333'
-					}
-				}
-				activeStyle={
-					{
-						border: '3px dashed #e74c3c'
-					}
-				} 
-				onDrop={this.onDrop.bind(this)}>
+				<Dropzone className="Uploader" onDrop={this.onDrop.bind(this)}>
 					{this.props.children}
 				</Dropzone>
 			</div>
