3206c53152972f4d3c76b74755aec1c848e7003b
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-19 11:31:00 +0530

    Added EditorPage and EditorComponent

---
+++ app/containers/EditorPage.js
@@ -0,0 +1,11 @@
+// @flow
+import React, { Component } from 'react';
+import EditorComponent from '../components/EditorComponent';
+
+export default class EditorPage extends Component {
+  render() {
+    return (
+      <EditorComponent />
+    );
+  }
+}
