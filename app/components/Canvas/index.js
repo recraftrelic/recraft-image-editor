f86ba3b9e57801ef892d2724f5cb35bf619feeb7
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-24 22:47:25 +0530

    added CanvasImage component

--- app/components/Canvas/index.js
+++ app/components/Canvas/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Layer, Stage} from 'react-konva';
+import CanvasImage from './image';
 
 class CanvasComponent extends React.Component {
 	render(){
 		return (
 			<Stage width={700} height={700}>
 				<Layer>
+					<CanvasImage image={this.props.bgImage} />
 				</Layer>
 			</Stage>
 		)
