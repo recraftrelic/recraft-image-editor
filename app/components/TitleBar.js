0fcb4a656018435c5a432c33cbf8cb3ba31a411d
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-02-26 13:21:47 +0530

    imported WindowControls

--- app/components/TitleBar.js
+++ app/components/TitleBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import WindowControls from './WindowControls';
 
 class TitleBar extends React.Component {
 	render () {
 		return (
 			<div className="full-width">
+				<div className="pull-left">
+					<WindowControls />
+				</div>
 				This is the titlebar
 			</div>
 		)
