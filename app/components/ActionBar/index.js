a1e9e75ba81c44c5c03483087e8ac011b797f958
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-04 18:03:58 +0530

    added ActionBarContainer

--- app/components/ActionBar/index.js
+++ app/components/ActionBar/index.js
@@ -4,19 +4,21 @@ import Icon from '../Icon';
 class ActionBar extends React.Component {
 	render () {
 		return (
-			<div id="ActionBar" className="btn-group">
-				<button className="btn btn-default">
-					<Icon name="undo" />
-				</button>
-				<button className="btn btn-default">
-					<Icon name="repeat" />
-				</button>
-				<button className="btn btn-default">
-					<Icon name="download" />
-				</button>
-				<button className="btn btn-default">
-					<Icon name="share-alt" />
-				</button>
+			<div id="ActionBarContainer" className="btn-group">
+				<div id="ActionBar">
+					<button className="btn btn-default">
+						<Icon name="undo" />
+					</button>
+					<button className="btn btn-default">
+						<Icon name="repeat" />
+					</button>
+					<button className="btn btn-default">
+						<Icon name="download" />
+					</button>
+					<button className="btn btn-default">
+						<Icon name="share-alt" />
+					</button>
+				</div>
 			</div>
 		)
 	}
