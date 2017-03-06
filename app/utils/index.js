1cad8ccce6d383df6b439fa01afa21fbd38a4dbc
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-07 00:41:47 +0530

    added saveImage

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,9 +1,17 @@
+import fs from 'fs';
+
 const calculateAspectRatioFit = (srcWidth, srcHeight, maxWidth, maxHeight) => {
 
     let ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
 
     return { width: srcWidth*ratio, height: srcHeight*ratio };
 
+}
+
+const saveImage = (filename, data) => {
+	fs.writeFile(filename, data, 'base64', (err) => {
+		alert(err);
+	})
 }
 
 export default {
