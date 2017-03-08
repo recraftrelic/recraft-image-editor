87185051222dae8f12233b055bc7edff09400012
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-08 23:07:19 +0530

    added reducers listners

--- app/reducers/canvas.js
+++ app/reducers/canvas.js
@@ -5,7 +5,10 @@ const initState = {
 	height: 0,
 	width: 0,
 	rotation: 0,
-	title: 'Upload a new image'
+	title: 'Upload a new image',
+	brightness: 0,
+	greyscale: 0,
+	sepia: 0
 };
 
 
@@ -17,6 +20,12 @@ export default (state = initState, action) => {
 			return {...state, image: action.image, height: action.height, width: action.width}
 		case actionsType.UPDATE_CANVS_TITLE:
 			return {...state, title: action.title}
+		case actionsType.UPDATE_BRIGHTNESS:
+			return {...state, brightness: action.brightness}
+		case actionsType.UPDATE_GREYSCALE:
+			return {...state, greyscale: action.greyscale}
+		case actionsType.UPDATE_SEPIA:
+			return {...state, sepia: action.sepia}
 		default:
 			return state;
 	}
